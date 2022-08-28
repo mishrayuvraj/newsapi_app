d331c563a84957f39f46c0d64eef0d33b9c72d0c
Yuvraj Mishra <myuvraj399@gmail.com>
2022-08-28 17:58:10 +0530

    resolved

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request=require("request");
-const product=require('./api/product');
+
 
 const https=require("https");
 const app = express();
@@ -74,11 +74,11 @@ app.get("/",function(req,response){
 
     });
 
-let port =process.env.PORT;
-if(port==null||port==""){
-  port=5000;
-}
-  app.use("/api/product",product);
+let port =process.env.PORT || 5000;
+// if(port==null||port==""){
+//   port=5000;
+// }
+  
     app.listen(port,function(){
         console.log("Server started successfully");
 
